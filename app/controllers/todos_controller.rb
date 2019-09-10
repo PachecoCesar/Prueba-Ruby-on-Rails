95bdee9a9f7aff4181b99f9276649a981f61b35f
PachecoCesar <c86pacheco@gmail.com>
2019-09-10 15:38:25 -0300

    identacion & refactoring done

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :edit, :update, :destroy, :complete]
   def index
     @todos = Todo.all
   end
@@ -12,28 +13,21 @@ class TodosController < ApplicationController
     redirect_to todos_path
   end
 
-  def show
-    @todo = Todo.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @todo = Todo.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @todo = Todo.find(params[:id])
     @todo.update(todo_params)
     redirect_to todos_path
   end
 
   def destroy
-    @todo = Todo.find(params[:id])
     @todo.delete
     redirect_to todos_path
   end
 
   def complete
-    @todo = Todo.find(params[:id])
     @todo.completed = true
     @todo.save
     redirect_to todos_path
@@ -47,5 +41,9 @@ class TodosController < ApplicationController
 
   def todo_params
       params.require(:todo).permit(:description, :completed)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
   end
 end
